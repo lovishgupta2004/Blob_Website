865baf4e7f4d57f777cf7acfbff671842e0d56da
lovishgupta2004 <guptalovish2004@gmail.com>
2026-05-07 00:50:17 +0530

    Changes

--- src/main/java/com/blog/blog/repositories/CategoryRepository.java
+++ src/main/java/com/blog/blog/repositories/CategoryRepository.java
@@ -2,10 +2,16 @@ package com.blog.blog.repositories;
 
 import com.blog.blog.domain.entities.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface CategoryRepository extends JpaRepository<Category, UUID> {
+
+    @Query("SELECT c FROM Category c LEFT JOIN FETCH c.posts")
+    List<Category> findAllWithPostCount();
+
 }
---
+++ src/main/java/com/blog/blog/service/CategoryService.java
@@ -0,0 +1,9 @@
+package com.blog.blog.service;
+
+import com.blog.blog.domain.entities.Category;
+
+import java.util.List;
+
+public interface CategoryService {
+    List<Category> listCategories();
+}
